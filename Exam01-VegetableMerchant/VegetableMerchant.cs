eee6eef362d626ca6051ed94def4771c9213f644
Svetlin <svetlinst@gmail.com>
2016-06-25 12:05:39 +0300

    VegetableMerchant added

---
+++ Exam01-VegetableMerchant/VegetableMerchant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class VegetableMerchant
+    {
+        static void Main()
+        {
+        double fruitPrice = double.Parse(Console.ReadLine());
+        double vegetablePrice = double.Parse(Console.ReadLine());
+        int fruitKiloSold = int.Parse(Console.ReadLine());
+        int vegetableKiloSold = int.Parse(Console.ReadLine());
+        double revenue = ((fruitPrice * fruitKiloSold) + (vegetablePrice * vegetableKiloSold)) / 1.94;
+        Console.WriteLine(revenue);
+        }
+    }
+
